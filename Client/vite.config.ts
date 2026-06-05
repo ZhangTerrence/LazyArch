36c808b4b89ab37de5e136bdaba3bdb9ffb604b4
Terrence Zhang <terrencezhang77@gmail.com>
2026-06-04 23:03:36 -0400

    added tanstack router

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from "vite";
-import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
+import tanstackRouter from "@tanstack/router-plugin/vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+	plugins: [
+		tanstackRouter({
+			target: "react",
+			autoCodeSplitting: true,
+			quoteStyle: "double",
+			routesDirectory: `${__dirname}/src/app/`,
+			generatedRouteTree: `${__dirname}/src/routeTree.gen.ts`,
+		}),
+		react(),
+		babel({ presets: [reactCompilerPreset()] }),
+	],
 	server: {
 		port: 3000,
 		strictPort: true,
