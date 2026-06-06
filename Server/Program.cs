eb5c5badaddbc1843f634a8b7eb601b896448ca9
Terrence Zhang <terrencezhang77@gmail.com>
2026-06-06 17:52:58 -0400

    added aspnetcore identity

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,8 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Server.Data;
+using Server.Data.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddIdentityApiEndpoints<User>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
